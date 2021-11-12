84b43f600a7932435bb7ce88105aa992340920ae
Diksha Sethi <30921554+dikshuuu@users.noreply.github.com>
2021-11-12 19:36:38 +0530

    Create cards.js

---
+++ non-components/cards.js
@@ -0,0 +1,32 @@
+export const CARDS = [
+  {
+    id: 0,
+    name: "Bank Draft",
+    image: "images/id0.jpg",
+    type: "bank-draft"
+  },
+  {
+    id: 1,
+    name: "Bill of Lading",
+    image: "images/id1.jpg",
+    type: "bill-of-lading"
+  },
+  {
+    id: 2,
+    name: "invoice",
+    image: "images/id2.jpg",
+    type: "invoice"
+  },
+  {
+    id: 3,
+    name: "Bank Draft2",
+    image: "images/id3.jpg",
+    type: "bank-draft-2"
+  },
+  {
+    id: 4,
+    name: "Bill ofLading2",
+    image: "images/id4.jpg",
+    type: "bill - of -lading - 2"
+  }
+];
