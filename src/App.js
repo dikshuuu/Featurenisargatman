02813bb5678faccba06fd31a146ea7fb75cb1bff
Diksha Sethi <30921554+dikshuuu@users.noreply.github.com>
2021-11-12 19:33:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import MainMenu from "./components/MainMenu";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MainMenu />
+      </div>
+    );
+  }
+}
+
+export default App;
